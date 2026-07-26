c0a5454b7d03fb917b028b66db3a8b0e26c868ad
Lildream6045 <charmanderfitbit@gmail.com>
2026-07-26 16:25:31 -0400

    Disabled the Practice Codes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 // Single Line Comment is the Code being pulled from the base of the FtcRobotController GitHub
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
-
+@Disabled
 @TeleOp
 public class HelloWorld extends OpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@Disabled
 @TeleOp
 public class IfPractice extends OpMode {
 
@@ -13,6 +15,23 @@ public class IfPractice extends OpMode {
 
     @Override
     public void loop() {
-
+        double leftY = gamepad1.left_stick_y;
+        boolean aButton = gamepad1.a;
+        //returns True or False
+        if (aButton) {
+            telemetry.addData("A Button", "Pressed");
+        }
+        else if (leftY > 0.5) {
+            telemetry.addData("Left Stick Y", leftY );
+        }
+        else {
+            telemetry.addData("A Button", "Not Pressed");
+        }
+        telemetry.addData("A Button State", aButton);
     }
+    /*
+    And = && if (leftY < 0.5 && leftY > 0) {
+    Or = || if (leftY < 0 || rightY < 0) {
+    Not= ! if (!clawClosed) {
+     */
 }
